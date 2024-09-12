c5c0cdc6b2f3a93a2f757fb04dc191816c9be457
Ikaterin-crtl <catarinaneedsajob@gmail.com>
2024-09-12 15:18:25 -0300

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menuToggle');
+    const navbar = document.getElementById('navbar');
+
+    menuToggle.addEventListener('click', function() {
+        navbar.classList.toggle('active');
+    });
+});
