5bb4373e489488f2651fbc05c73dfeddf30cffd4
Ikaterin-crtl <catarinaneedsajob@gmail.com>
2024-09-25 09:59:12 -0300

    commit alterações html formulario

--- script.js
+++ script.js
@@ -9,4 +9,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+document.querySelector('#maintenanceForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+
+    const formData = new FormData(this); // Coleta os dados do formulário
+
+    fetch(this.action, {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => {
+        if (response.ok) {
+            alert('Obrigado por entrar em contato! Seu formulário foi enviado.');
+            this.reset(); // Limpa o formulário após o envio
+        } else {
+            alert('Desculpe, houve um erro ao enviar o formulário. Tente novamente.');
+        }
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+        alert('Desculpe, houve um erro ao enviar o formulário. Tente novamente.');
+    });
+});
 
